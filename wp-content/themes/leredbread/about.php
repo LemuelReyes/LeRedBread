0209195996010349201220a6f9248aa6929c278e
lemuel <reyeslemuel@hotmail.com>
2016-03-11 13:53:49 -0800

    add categories, hr symbol to product page. format grid too.

--- wp-content/themes/leredbread/about.php
+++ wp-content/themes/leredbread/about.php
@@ -13,7 +13,7 @@ get_header();
       <h2 class="flex-text-center"><?php echo CFS()->get( 'tagline' ); ?></h2>
       <p class="flex-text-center">We are a team of creative and talented individuals who love making delicious treats.</p>
 
-      <div class="about-section">
+      <div class="about-section container">
       <!-- Team section  -->
       <div class="about-column-block">
       <img src="<?php bloginfo( 'template_url' ); ?>/images/team.jpg" alt="Le Bread Team picture" class="team-picture"/>
@@ -26,13 +26,13 @@ get_header();
       <img src="<?php bloginfo( 'template_url' ); ?>/images/bakery.jpg" alt="picture of Le Bread facility" class="bakery-picture"/>
       <h2>Le Red Bread Bakery</h2>
       <?php echo CFS()->get( 'bakery_copy' ); ?>
-      <h2 class="flex-text-center">Our Story</h2>
       </div>
       </div> <!-- ends about section class -->
 
       <!--  Story Section -->
       <section class="flex-middle">
       <div class="container">
+        <h2 class="flex-text-center">Our Story</h2
       <?php echo CFS()->get( 'our_story_copy' ); ?>
       </div>
       </section>
--- wp-content/themes/leredbread/archive-product.php
+++ wp-content/themes/leredbread/archive-product.php
@@ -14,24 +14,48 @@ get_header(); ?>
 
 			<header class="page-header">
 				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_title( '<h1 class="page-title flex-text-center">', '</h1>' );
 					the_archive_description( '<div class="taxonomy-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
+
+			<hr class="hr-symbol" />
+			<div class="grey-dot"></div>
+
+			<!-- CATEGORIES  -->
+				<?php
+				$terms= get_terms( 'product-type', $args);
+				?>
+
+				<section class="product-info">
+					<div class="product-flex">
+							<?php if ( ! empty( $terms ) ) : ?>
+									<?php foreach ($terms as $term) : ?>
+													<div class="product-container">
+															<img src="<?php echo get_template_directory_uri() . '/images\/' . $term->slug; ?>.png" alt="" />
+															<h3><?php echo $term->name; ?></h3>
+													</div>
+									<?php endforeach; ?>
+							<?php endif; ?>
+					</div>
+			</section>
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 
       <!--  CREATE A PRODUCT SQUARE -->
-
+			<div class="product-grid">
       <?php if ( has_post_thumbnail() ) : ?>
         <?php the_post_thumbnail( 'medium' ); ?>
       <?php endif; ?>
 
-      <?php the_title() ?>
+			<div class="flex-product-description">
+      <span><?php the_title() ?></span>
 
-      <?php echo CFS()->get( 'price' ); ?>
+      <span><?php echo CFS()->get( 'price' ); ?></span>
+			</div>
+			</div> <!-- ends flex product description -->
 
 			<?php endwhile; ?>
 
