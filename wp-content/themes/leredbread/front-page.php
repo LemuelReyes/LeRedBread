357d28f7d310981872724a044e424d0ca2a94aea
lemuel <reyeslemuel@hotmail.com>
2016-03-12 22:17:57 -0800

    format categories infront page

--- wp-content/themes/leredbread/front-page.php
+++ wp-content/themes/leredbread/front-page.php
@@ -26,7 +26,7 @@ get_header(); ?>
                     <?php foreach ($terms as $term) : ?>
                             <div class="product-container">
                                 <img src="<?php echo get_template_directory_uri() . '/images\/' . $term->slug; ?>.png" alt="" />
-                                <h3><?php echo $term->name; ?></h3>
+                                <h3 class="category-text"><?php echo $term->name; ?></h3>
                                 <p>
                                     <?php echo $term->description; ?> <a href="<?php echo get_term_link( $term ); ?>">See More...</a>
                                 </p>
@@ -46,6 +46,8 @@ get_header(); ?>
 
 
 				<!-- BLOG POSTS  -->
+		<section class="latest-blogs">
+			<div class="container">
 				<div class="flex-text-center">
 					<h2>Our Latest News</h2>
 				</div>
@@ -53,7 +55,6 @@ get_header(); ?>
 				<hr class="hr-symbol" />
 				<div class="grey-dot"></div>
 
-				<div class="flex-post container">
 					<?php
 			          $args = array( 'post_type' => 'post',
 			                         'posts_per_page' => 4
@@ -66,27 +67,31 @@ get_header(); ?>
 							<ul>
 								<li>
 									<div class="thumbnail-container">
-	              <?php if (has_post_thumbnail() ) : ?>
-	                <?php the_post_thumbnail( 'small' ); ?>
-	              <?php endif; ?>
+					              <?php if (has_post_thumbnail() ) : ?>
+					                <?php the_post_thumbnail( 'small' ); ?>
+					              <?php endif; ?>
 									</div>
+
 	              <!--  title -->
 
-	              <h3>
-	                <a href="<?php the_permalink(); ?>">
-	                        <?php the_title(); ?>
-	                </a>
-	              </h3>
+									<div class="blog-post-info">
+					              <h3>
+						                <a href="<?php the_permalink(); ?>">
+						                         <?php the_title(); ?>
+						                </a>
+					              </h3>
 
-          <span class="entry-meta">
-                <?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
-          </span>
+						          <span class="entry-meta">
+						                <?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
+						          </span>
+									</div>
 								</li>
 							</ul>
           <?php endforeach; wp_reset_postdata(); ?>
-				</div>
-		</main><!-- #main -->
 
+				</div> <!-- ends container div -->
+			</section>	<!-- ends blog section -->
+		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php get_footer(); ?>
